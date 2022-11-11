a3e98d10100d1be78127117b4de0e6704421d4bb
semako123 <zosusemenoch@gmail.com>
2022-11-11 14:40:37 +0100

    Create testimonialCard component Ad testimonial cards to testimonial section

---
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./testimonialCard.css";
+import star from "../assets/images/star.png";
+
+const TestimonialCard = ({
+  children,
+  image,
+  quote,
+  name,
+  companyName,
+  stars,
+}) => {
+  const starsList = Array(stars).fill(" ");
+
+  return (
+    <div className="testimonial-card">
+      <div className="testimonial-card-header">
+        <img className="testimonial-card-profile" src={image} alt={name} />
+        <div className="testimonial-card-header-texts">
+          <p className="testimonial-card-name">{name}</p>
+          <p className="testimonial-card-company-name">{companyName}</p>
+        </div>
+      </div>
+      <h3 className="testimonial-card-quote">"{quote}" </h3>
+      <p className="testimonial-card-detail">{children}</p>
+      <div>
+        {starsList.map((element) => (
+          <img className="starIcon" src={star} alt="star" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- src/components/testiminonialSection/Testimonial.jsx
+++ src/components/testiminonialSection/Testimonial.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./testimonial.css";
 import SectionBadge from "../sectionBadge/SectionBadge";
 import Button from "../button/button";
+import profile1 from "../assets/images/prof1.jpg"
+import profile2 from "../assets/images/prof2.jpg";
+import profile3 from "../assets/images/prof3.jpg";
+import TestimonialCard from "../TestimonialCard/TestimonialCard";
 
 const Testimonial = () => {
   return (
@@ -18,6 +22,44 @@ const Testimonial = () => {
         <div className="testimonial-para-button">
           <Button type="primary">Get in touch</Button>
         </div>
+      </div>
+      <div className="testimonial-cards-section">
+        <TestimonialCard
+          quote="I recommend this agency"
+          name="John Doe"
+          companyName="Company name"
+          image={profile3}
+          stars={5}
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+          voluptas, asperiores saepe hic nemo itaque dolor at ex voluptate?
+          Quidem voluptatibus adipisci necessitatibus, aspernatur illum quia
+          eius architecto quo possimus!
+        </TestimonialCard>
+        <TestimonialCard
+          quote="The support is awesome"
+          name="Alice Smith"
+          companyName="Company name"
+          image={profile2}
+          stars={5}
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+          voluptas, asperiores saepe hic nemo itaque dolor at ex voluptate?
+          Quidem voluptatibus adipisci necessitatibus, aspernatur illum quia
+          eius architecto quo possimus!
+        </TestimonialCard>
+        <TestimonialCard
+          quote="A game changer for us"
+          name="Barry Smith"
+          companyName="Company name"
+          image={profile1}
+          stars={5}
+        >
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque
+          voluptas, asperiores saepe hic nemo itaque dolor at ex voluptate?
+          Quidem voluptatibus adipisci necessitatibus, aspernatur illum quia
+          eius architecto quo possimus!
+        </TestimonialCard>
       </div>
     </div>
   );
