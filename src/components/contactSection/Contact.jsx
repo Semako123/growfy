6303a2effc1918e7b217a34b64b7e6fef5f91762
semako123 <zosusemenoch@gmail.com>
2022-11-12 21:14:54 +0100

    Add animation to contact section

--- src/components/contactSection/Contact.jsx
+++ src/components/contactSection/Contact.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import "./contact.css";
 import Button from "../button/button";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Contact = () => {
   return (
-    <div className="contact-section">
-      <div className="contact-texts">
-        <h2 className="contact-topic">
-          Ready to start scaling your business now?
-        </h2>
-        <p className="contact-para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          fuga error laudantium alias molestiae soluta nostrum saepe quaerat
-          nisi.
-        </p>
+    <AnimationOnScroll animateIn="animate__rubberBand" animateOut="animate__fadeOut">
+      <div className="contact-section">
+        <div className="contact-texts">
+          <h2 className="contact-topic">
+            Ready to start scaling your business now?
+          </h2>
+          <p className="contact-para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            fuga error laudantium alias molestiae soluta nostrum saepe quaerat
+            nisi.
+          </p>
+        </div>
+        <div className="contact-buttons">
+          <Button type="primary">Our services</Button>
+          <Button type="secondary">Contact us</Button>
+        </div>
       </div>
-          <div className="contact-buttons">
-        <Button type="primary">Our services</Button>
-        <Button type="secondary">Contact us</Button>
-      </div>
-    </div>
+    </AnimationOnScroll>
   );
 };
 
