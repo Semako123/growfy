62ad2ebe3bd90f8ec7f6642861c519c6fba41890
semako123 <zosusemenoch@gmail.com>
2022-11-12 18:25:21 +0100

    Add animations to rating section

--- src/components/RatingSection/Ratings.jsx
+++ src/components/RatingSection/Ratings.jsx
@@ -1,38 +1,64 @@
 import React from "react";
 import "./ratings.css";
 import RatingsCard from "../ratingsCard/RatingsCard";
-import bg0 from "../assets/images/bg7.jpg"
+import bg0 from "../assets/images/bg7.jpg";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const Ratings = () => {
   return (
     <div className="ratings-section">
       <div className="rating-card-grid">
-        <RatingsCard heading="1.2M+">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-          optio accusantium quae velit excepturi fuga ea at molestias recusandae
-          dolor.
-        </RatingsCard>
-        <RatingsCard heading="3.4B">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-          optio accusantium quae velit excepturi fuga ea at molestias recusandae
-          dolor.
-        </RatingsCard>
+        <AnimationOnScroll
+          animateIn="animate__bounceIn"
+          animateOut="animate__bounceOut"
+        >
+          <RatingsCard heading="1.2M+">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+            optio accusantium quae velit excepturi fuga ea at molestias
+            recusandae dolor.
+          </RatingsCard>
+        </AnimationOnScroll>
+        <AnimationOnScroll
+          animateIn="animate__bounceIn"
+          animateOut="animate__bounceOut"
+        >
+          <RatingsCard heading="3.4B">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+            optio accusantium quae velit excepturi fuga ea at molestias
+            recusandae dolor.
+          </RatingsCard>
+        </AnimationOnScroll>
       </div>
       <div className="rating-card-grid">
-        <RatingsCard heading="98%">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-          optio accusantium quae velit excepturi fuga ea at molestias recusandae
-          dolor.
-        </RatingsCard>
-        <RatingsCard heading="10+">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
-          optio accusantium quae velit excepturi fuga ea at molestias recusandae
-          dolor.
-        </RatingsCard>
+        <AnimationOnScroll
+          animateIn="animate__bounceIn"
+          animateOut="animate__bounceOut"
+        >
+          <RatingsCard heading="98%">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+            optio accusantium quae velit excepturi fuga ea at molestias
+            recusandae dolor.
+          </RatingsCard>
+        </AnimationOnScroll>
+        <AnimationOnScroll
+          animateIn="animate__bounceIn"
+          animateOut="animate__bounceOut"
+        >
+          <RatingsCard heading="10+">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+            optio accusantium quae velit excepturi fuga ea at molestias
+            recusandae dolor.
+          </RatingsCard>
+        </AnimationOnScroll>
       </div>
-      <div className="rating-image-div">
-        <img className="rating-image" src={bg0} alt="growfy" />
-      </div>
+      <AnimationOnScroll
+        animateIn="animate__bounceIn"
+        animateOut="animate__bounceOut"
+      >
+        <div className="rating-image-div">
+          <img className="rating-image" src={bg0} alt="growfy" />
+        </div>
+      </AnimationOnScroll>
     </div>
   );
 };
